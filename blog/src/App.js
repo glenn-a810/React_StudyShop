1411645b999e533822d4e1e098379025d634787e
Glenn <a810.glenn@gmail.com>
2022-01-22 20:35:52 +0900

    않이 vue 고작 고거했다고 react state/props를 다 까먹었네 ㅅㅂ

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 function App() {
   let [title, titleEdit] = useState(['React state', 'Vue state', 'Dart?'])
-  let [like, likeEdit] = useState(0)
+  let [like, likeEdit] = useState([0,0,0])
   let [modal, modalEdit] = useState(false)
   // const posts = 'React vs Vue';
 
@@ -18,33 +18,43 @@ function App() {
       <div className="black-nav">
         <div>Dev Blog</div>
       </div>
-      <div className="list">
-        <h3>
-          {title[0]}{' '}
-          <span
-            onClick={() => {
-              likeEdit(like++)
-            }}
-          >
-            {' '}
-            👍{' '}
-          </span>{' '}
-          {like}
-        </h3>
-        <p>1월 15일 발행</p>
-        {/*<button onClick={titleChange}>제목 변경</button>*/}
-        <hr />
-      </div>
-      <div className="list">
-        <h3>{title[1]}</h3>
-        <p>1월 15일 발행</p>
-        <hr />
-      </div>
-      <div className="list">
-        <h3>{title[2]}</h3>
-        <p>1월 15일 발행</p>
-        <hr />
-      </div>
+      {/*<div className="list">*/}
+      {/*  <h3>*/}
+      {/*    {title[0]}{' '}*/}
+      {/*    <span*/}
+      {/*      onClick={() => {*/}
+      {/*        likeEdit(like++)*/}
+      {/*      }}*/}
+      {/*    >*/}
+      {/*      {' '}*/}
+      {/*      👍{' '}*/}
+      {/*    </span>{' '}*/}
+      {/*    {like}*/}
+      {/*  </h3>*/}
+      {/*  <p>1월 15일 발행</p>*/}
+      {/*  /!*<button onClick={titleChange}>제목 변경</button>*!/*/}
+      {/*  <hr />*/}
+      {/*</div>*/}
+      {/*<div className="list">*/}
+      {/*  <h3>{title[1]}</h3>*/}
+      {/*  <p>1월 15일 발행</p>*/}
+      {/*  <hr />*/}
+      {/*</div>*/}
+      {/*<div className="list">*/}
+      {/*  <h3>{title[2]}</h3>*/}
+      {/*  <p>1월 15일 발행</p>*/}
+      {/*  <hr />*/}
+      {/*</div>*/}
+
+      {title.map((key) => {
+        return (
+          <div className="list">
+            <h3>{key}</h3>
+            <p>1월 15일 발행</p>
+            <hr />
+          </div>
+        )
+      })}
 
       <button
         onClick={() => {
@@ -54,17 +64,18 @@ function App() {
         Modal
       </button>
 
-      {modal === true ? <Modal /> : null}
+      {modal === true ? <Modal title={title} /> : null}
 
-      {/*<Modal />*/}
+      {/*/!*<Modal />*!/*/}
+
     </div>
   )
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h2>제목</h2>
+      <h2>{props.title[0]}</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
