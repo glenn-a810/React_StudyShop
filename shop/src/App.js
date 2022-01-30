ecb124fa92ec83488e54b571e0a81c0819760459
Glenn <a810.glenn@gmail.com>
2022-01-30 22:36:58 +0900

    레이아웃 세팅

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,10 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <button type="button" className="btn btn-primary">Primary</button>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Dev Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="jumbotron">
+        <h1>20% Season Off</h1>
+        <p>
+          This is a simple unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="100%"
+              alt="1"
+            />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="100%"
+              alt="2"
+            />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="100%"
+              alt="3"
+            />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
