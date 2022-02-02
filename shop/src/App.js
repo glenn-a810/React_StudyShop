a5c81d95b51b97676580dae484867752be60f7bf
Glenn <a810.glenn@gmail.com>
2022-02-02 21:02:04 +0900

    React Router 설치, 잘되는지 테스트. 어..이건 router폴더에서 했었던거 같은데..?

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { useState } from "react";
 import data from "./asset/data";
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   const [shoes, shoesEdit] = useState(data);
@@ -19,54 +20,45 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="jumbotron">
-        <h1>20% Season Off</h1>
-        <p>
-          This is a simple unit, a simple jumbotron-style component for calling
-          extra attention to featured content or information.
-        </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
-      </div>
-
-      <div className="container">
-        <div className="row">
-          {/*<div className="col-md-4">*/}
-          {/*  <img*/}
-          {/*    src="https://codingapple1.github.io/shop/shoes1.jpg"*/}
-          {/*    width="100%"*/}
-          {/*    alt="1"*/}
-          {/*  />*/}
-          {/*  <h4>{shoes[0].title}</h4>*/}
-          {/*  <p>{shoes[0].content}</p>*/}
-          {/*  <p>{shoes[0].price}</p>*/}
-          {/*</div>*/}
-          {/*<div className="col-md-4">*/}
-          {/*  <img*/}
-          {/*    src="https://codingapple1.github.io/shop/shoes2.jpg"*/}
-          {/*    width="100%"*/}
-          {/*    alt="2"*/}
-          {/*  />*/}
-          {/*  <h4>{shoes[1].title}</h4>*/}
-          {/*  <p>{shoes[1].content}</p>*/}
-          {/*  <p>{shoes[1].price}</p>*/}
-          {/*</div>*/}
-          {/*<div className="col-md-4">*/}
-          {/*  <img*/}
-          {/*    src="https://codingapple1.github.io/shop/shoes3.jpg"*/}
-          {/*    width="100%"*/}
-          {/*    alt="3"*/}
-          {/*  />*/}
-          {/*  <h4>{shoes[2].title}</h4>*/}
-          {/*  <p>{shoes[2].content}</p>*/}
-          {/*  <p>{shoes[2].price}</p>*/}
-          {/*</div>*/}
-          {data.map((key, id) => {
-            return <Item data={data[id]} id={id} />;
-          })}
+      <Route exact path="/">
+        <div className="jumbotron">
+          <h1>20% Season Off</h1>
+          <p>
+            This is a simple unit, a simple jumbotron-style component for
+            calling extra attention to featured content or information.
+          </p>
+          <p>
+            <Button variant="primary">Learn more</Button>
+          </p>
         </div>
-      </div>
+
+        <div className="container">
+          <div className="row">
+            {data.map((key, id) => {
+              return <Item data={data[id]} id={id} />;
+            })}
+          </div>
+        </div>
+      </Route>
+      <Route path="/detail">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img
+                src="https://codingapple1.github.io/shop/shoes1.jpg"
+                width="100%"
+                alt="shoes1"
+              />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button>
+            </div>
+          </div>
+        </div>
+      </Route>
     </div>
   );
 
@@ -74,9 +66,13 @@ function App() {
     return (
       <div className="col-md-4">
         <img
-          src={"https://codingapple1.github.io/shop/shoes" + (props.id+1) + ".jpg"}
+          src={
+            "https://codingapple1.github.io/shop/shoes" +
+            (props.id + 1) +
+            ".jpg"
+          }
           width="100%"
-          alt={ props.key }
+          alt={props.key}
         />
         <h4>{props.data.title}</h4>
         <p>{props.data.content}</p>
