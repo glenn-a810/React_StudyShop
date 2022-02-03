02cfc0695cb6b67ddc704b7a0efa1d4105324f8b
Glenn <a810.glenn@gmail.com>
2022-02-03 21:27:46 +0900

    react-route 기본적인거 복습. switch는 감이 잘 안오긴 한다만..

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,62 +3,58 @@ import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { useState } from "react";
 import data from "./asset/data";
 import { Link, Route, Switch } from "react-router-dom";
+import Detail from "./Detail";
 
 function App() {
-  const [shoes, shoesEdit] = useState(data);
+  // const [shoes, shoesEdit] = useState(data);
 
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Dev Shop</Navbar.Brand>
+          <Navbar.Brand>Dev Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/detail">Detail</Link>
+            </Nav.Link>
+            <Nav.Link>Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <Route exact path="/">
-        <div className="jumbotron">
-          <h1>20% Season Off</h1>
-          <p>
-            This is a simple unit, a simple jumbotron-style component for
-            calling extra attention to featured content or information.
-          </p>
-          <p>
-            <Button variant="primary">Learn more</Button>
-          </p>
-        </div>
+      <Switch>
+        <Route exact path="/">
+          <div className="jumbotron">
+            <h1>20% Season Off</h1>
+            <p>
+              This is a simple unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
+            </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </div>
 
-        <div className="container">
-          <div className="row">
-            {data.map((key, id) => {
-              return <Item data={data[id]} id={id} />;
-            })}
-          </div>
-        </div>
-      </Route>
-      <Route path="/detail">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img
-                src="https://codingapple1.github.io/shop/shoes1.jpg"
-                width="100%"
-                alt="shoes1"
-              />
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className="btn btn-danger">주문하기</button>
+          <div className="container">
+            <div className="row">
+              {data.map((key, id) => {
+                return <Item data={data[id]} id={id} />;
+              })}
             </div>
           </div>
-        </div>
-      </Route>
+        </Route>
+
+        <Route path="/detail">
+          <Detail />
+        </Route>
+
+        <Route path="/:id">
+          <div>아무거나 적었을 때 이거 보여짐</div>
+        </Route>
+      </Switch>
     </div>
   );
 
