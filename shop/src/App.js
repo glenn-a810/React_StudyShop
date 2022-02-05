dce8b1605e3bf9758e2b14f9b328c160431db12b
Glenn <a810.glenn@gmail.com>
2022-02-05 20:26:28 +0900

    data외부 사용시 변경사항, state 사용 시 달라지는 점 비교

--- shop/src/App.js
+++ shop/src/App.js
@@ -49,6 +49,7 @@ function App() {
 
         <Route path="/detail/:params">
           <Detail shoes={shoes} />
+          {/*<Detail shoes={data} />*/}
         </Route>
 
         <Route path="/:id">
