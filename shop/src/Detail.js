28646644d5c70531a1d47e4e5b1b41b2ebd415b8
Glenn <a810.glenn@gmail.com>
2022-02-06 21:21:22 +0900

    styled components 간단한 사용법, props를 사용해서 스타일링 적용하는 법. 이런건 못봤던거 같은데??

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,16 +1,28 @@
 import React from "react";
 import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+let Box = styled.div`
+  padding: 20px;
+`;
+
+let Title = styled.h4`
+  font-size: 25px;
+  color: ${(props) => props.textColor};
+`;
 
 function Detail(props) {
   const history = useHistory();
   const { params } = useParams();
 
-  const findItemId = props.shoes.find((item) => {
-    return item.id == params;
-  });
+  const findItemId = props.shoes.find(item => item.id == params);
 
   return (
     <div className="container">
+      <Box>
+        <Title textColor={"red"}>Detail</Title>
+        <Title textColor="blue">Detail</Title>
+      </Box>
       <div className="row">
         <div className="col-md-6">
           <img
