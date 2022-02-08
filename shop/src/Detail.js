7507933ea63249a7e602671a71724b362aae8e26
Glenn <a810.glenn@gmail.com>
2022-02-08 21:34:57 +0900

    SASS 간단 사용법. 이게 아무리 좋고 편해도 결국 styled components쓴다는거군

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,14 +1,33 @@
 import React from "react";
 import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
+import "./Detail.scss";
+
+let Box = styled.div`
+  padding-top: 30px;
+`;
+
+let Title = styled.h4`
+  font-size: 25px;
+  color: ${(props) => props.color};
+`;
 
 function Detail(props) {
   const history = useHistory();
   const { params } = useParams();
 
-  const findItemId = props.shoes.find(item => item.id == params);
+  const findItemId = props.shoes.find((item) => item.id == params);
 
   return (
     <div className="container">
+      <Box>
+        <Title className="red">Detail</Title>
+      </Box>
+
+      <div className="detailAlert-yellow">
+        <p>재고가 얼마 남지 않았습니다.</p>
+      </div>
+
       <div className="row">
         <div className="col-md-6">
           <img
